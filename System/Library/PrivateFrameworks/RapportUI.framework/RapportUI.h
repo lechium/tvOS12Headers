42200fae8d53a0724737082a6fe342cbb68d07a0
Kevin Bradley <kevin.w.bradley@me.com>
2019-05-31 11:41:11 -0700

    first commit

---
+++ System/Library/PrivateFrameworks/RapportUI.framework/RapportUI.h
@@ -0,0 +1,5 @@
+#import <RapportUI/RPUICloudServer.h>
+#import <RapportUI/RPUICloudSession.h>
+#import <RapportUI/RPHighlightButton.h>
+#import <RapportUI/RPPairingManagerUIController.h>
+#import <RapportUI/RPPairingUIController.h>
